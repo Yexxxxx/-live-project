5c7a4e3109674d7712ee7d4dcb9fe27663949a60
Yexxxxx <Yexxxxx@users.noreply.github.com>
2020-04-08 20:06:15 +0800

    最终结果-1

--- out.py
+++ out.py
@@ -2,6 +2,7 @@ from tkinter import *
 from operator import itemgetter
 import stureg
 import datetime
+import tkinter.messagebox
 students = stureg.get_value()
 print(students)
 def find():
@@ -21,8 +22,12 @@ def result(e2):
         mid = students[i]
         mid_no = mid[1]
         if (num == mid_no):
+            if (mid[3] != "未离开"):
+                tkinter.messagebox.showinfo('提示', '请先入校登记')
+                break
             outTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
             mid[3] = outTime
+            tkinter.messagebox.showinfo('提示', '离校成功，数据更新')
             break
         else:
-            print("查无此人")
+            tkinter.messagebox.showinfo('提示','查无此人')
--- stureg.py
+++ stureg.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from operator import itemgetter
 import datetime
+import tkinter.messagebox
 students = []
 def into():
     root = Tk()
@@ -14,26 +15,30 @@ def into():
     e2 = Entry(root, textvariable=p1)
     e1.grid(row=0, column=1, padx=10, pady=5)  # 设置输入框显示的位置，以及长和宽属性
     e2.grid(row=1, column=1, padx=10, pady=5)
-    Button(root, text='登入', width=10, command= lambda: show(e1.get(), e2.get())) \
+    Button(root, text='登入', width=10, command= lambda: get(e1.get(), e2.get())) \
         .grid(row=2, column=0, sticky=W, padx=10, pady=5)
     mainloop()
-def show(e1,e2):
+def get(e1,e2):
     nowTime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    outTime = 0000
+    outTime = "未离开"
     student=[e1,e2,nowTime,outTime]
     students.append(student)
+    tkinter.messagebox.showinfo('提示', '登记成功')
 def look():
     root = Tk()
     root.title('学生信息')
-    text = Text(root, width=400, height=400)
+    text = Text(root, width=120, height=120)
     text.pack(fill=X, side=BOTTOM)
-    text.insert(END,students)  # INSERT表示在光标位置插入
-    text.see(END)
-    text.update()
-    print("名字               学号               进入时间                离开时间")
+    colume = ["名字：","  学号：","  进入时间：","  离开时间："]
     student = sorted(students, key=itemgetter(1))
     for x in student:
-        print(x)
+        for i in range (0,4):
+            text.insert(END,colume[i])
+            text.insert(END,x[i])
+        text.insert(END,'\n')# INSERT表示在光标位置插入
+    text.see(END)
+    text.update()
+
 
 def get_value():
     return students
