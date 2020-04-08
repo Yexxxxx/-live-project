6d051802d0e23613c51d2b667ecfaa4ffa3094c0
Goi-27 <60462762+Goi-27@users.noreply.github.com>
2020-04-08 16:22:49 +0800

    Update stureg.py
    更改显示内容

--- stureg.py
+++ stureg.py
@@ -7,14 +7,14 @@ Label2 = Label(root, text='学号:').grid(row=1, column=0)
 v1 = StringVar()
 p1 = StringVar()
 e1 = Entry(root, textvariable=v1)  # Entry 是 Tkinter 用来接收字符串等输入的控件.
-e2 = Entry(root, textvariable=p1, show='#')
+e2 = Entry(root, textvariable=p1)
 e1.grid(row=0, column=1, padx=10, pady=5)  # 设置输入框显示的位置，以及长和宽属性
 e2.grid(row=1, column=1, padx=10, pady=5)
 
 
 def show():
-    print("会员名称:%s" % e1.get())  # 获取用户输入的信息
-    print("会员代号:%s" % e2.get())
+    print("学生姓名:%s" % e1.get())  # 获取用户输入的信息
+    print("学生学号:%s" % e2.get())
 
 
 Button(root, text='登入', width=10, command=show) \
